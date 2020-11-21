203dbd297d0a8dadf68d726d78b4e7ede672fff5
Manuel García Solipa <manuel.garcia@ebolution.com>
2020-11-21 18:57:46 +0100

    Add n98-magerun to php images

--- php/7.0-fpm/docker-entrypoint.sh
+++ php/7.0-fpm/docker-entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -e
 
+if [ -f /var/www/conf/php/php-fpm.conf ]; then
+        sudo cp -f /var/www/conf/php/php-fpm.conf /usr/local/etc/
+fi
+
+if [ -f /var/www/conf/php/php.ini ]; then
+        sudo cp -f /var/www/conf/php/php.ini /usr/local/etc/php/
+fi
+
 sudo chown -R app:app /var/www
 
 # first arg is `-f` or `--some-option`
